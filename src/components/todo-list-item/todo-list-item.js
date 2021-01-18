9c5c1de9d616314fe2c22724585fe87e2cfaa2f6
Jenya Yacovets <jenya.yacovets@gmail.com>
2021-01-18 14:31:59 +0300

    Add state classes, react icons, onClick functions

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react'
+import { AlertIcon } from '@primer/octicons-react'
+
+import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 
-    render() {
+    // Аналогичен с кодом ниже
+    // constructor() {
+    //     super()
 
+    //     this.state = {
+    //         done: false
+    //     }
+
+    //     this.onLabelClick = () => {
+    //         console.log(`Done: ${this.props.label}`)
+    //     }
+    // }
+
+    state = {
+        done: false,
+        status: this.props.status
+    }
+
+    onLabelClick = () => {
+
+        const { done } = this.state
+        let newDone
+
+        if (done) {
+            newDone = false
+        } else {
+            newDone = true
+        }
+
+        this.setState({
+            done: newDone
+        })
+    }
+
+    onMarkImportant = () => {
+
+        const { status } = this.state
+        let newStatus
+
+        if (status === 2) {
+            newStatus = 1
+        } else {
+            newStatus = 2
+        }
+
+        this.setState({
+            status: newStatus
+        })
+    }
+
+    render() {
         const { label } = this.props
+        const { done, status } = this.state
+
+        const style = {
+            textDecoration: done ? 'line-through' : 'none'
+        }
+
+        let className = 'itemTodo'
+
+        if (status === 2) {
+            className += ' important'
+        }
 
         return (
-            <span>{label}</span>
+            <span>
+                <span 
+                className={className}
+                style={style}
+                onClick={this.onLabelClick} >{label}</span>
+
+                <button 
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant} >
+                    <AlertIcon aria-label="Отметить как важное" />
+                </button>
+            </span>
         )
     }
 }
